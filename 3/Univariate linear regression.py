341458fac7050b8aec2d9b9dba5ff173247841e4
yushb0602 <yushb0602@gmail.com>
2018-03-04 19:11:56 +0800

    lineworks

--- 3/Univariate linear regression.py
+++ 3/Univariate linear regression.py
@@ -1,16 +1,3 @@
-import matplotlib.pyplot as plt # import matplotlib
-import numpy as np # import numpy
-import tensorflow as tf
-import numpy as np
-
-trX = np.linspace(-1, 1, 101) #Create a linear space of 101 points between 1 and 1
-trY = 2 * trX + np.random.randn(*trX.shape) * 0.4 + 0.2 #Create The y function based on the x axis
-plt.figure() # Create a new figure
-plt.scatter(trX,trY) #Plot a scatter draw of the random datapoints
-plt.plot (trX, .2 + 2 * trX) # Draw one line with the line function
-
-get_ipython().magic('matplotlib inline')
-
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
@@ -20,14 +7,16 @@ trY = 2 * trX + np.random.randn(*trX.shape) * 0.4 + 0.2 # create a y value which
 
 plt.scatter(trX,trY)
 plt.plot (trX, .2 + 2 * trX)
+plt.show()
 
+#...
 X = tf.placeholder("float", name="X") # create symbolic variables
 Y = tf.placeholder("float", name = "Y")
 
 with tf.name_scope("Model"):
 
     def model(X, w, b):
-        return tf.mul(X, w) + b # We just define the line as X*w + b0  
+        return tf.multiply(X, w) + b # We just define the line as X*w + b0  
 
     w = tf.Variable(-1.0, name="b0") # create a shared variable
     b = tf.Variable(-2.0, name="b1") # create a shared variable
@@ -41,12 +30,12 @@ train_op = tf.train.GradientDescentOptimizer(0.05).minimize(cost)
 
 
 sess = tf.Session()
-init = tf.initialize_all_variables()
-tf.train.write_graph(sess.graph, '/home/ubuntu/linear','graph.pbtxt')
-cost_op = tf.scalar_summary("loss", cost)
-merged = tf.merge_all_summaries()
+init = tf.global_variables_initializer()
+tf.train.write_graph(sess.graph, '/tmp/linear','graph.pbtxt')
+cost_op = tf.summary.scalar("loss", cost)
+merged = tf.summary.merge_all()
 sess.run(init)
-writer = tf.train.SummaryWriter('/home/ubuntu/linear', sess.graph)
+writer = tf.summary.FileWriter('/tmp/linear', sess.graph)
 
 for i in range(100):
     for (x, y) in zip(trX, trY): 
@@ -64,4 +53,4 @@ print((sess.run(b))) #Should be around 0.2
 
 plt.scatter(trX,trY)
 plt.plot (trX, sess.run(b) + trX * sess.run(w))
-
+plt.show()
